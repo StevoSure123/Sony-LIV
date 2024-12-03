4977e9d0f128ab2c143fa2e976fedf809e220144
StevoSure123 <stevosure123@gmail.com>
2024-12-03 09:33:17 +0600

    Update play.m3u8.js

--- api/sony-liv/play.m3u8.js
+++ api/sony-liv/play.m3u8.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       return;
     }
 
-    const originalUrl = `https://live.drmlive-02.workers.dev/sliv/id=${id}.m3u8`;
+    const originalUrl = `https://fifabd.xyz/OPLLX/play.m3u8?id=${id}`;
 
     // Fetch the M3U8 playlist from the original URL
     const response = await fetch(originalUrl, {
